615eaf425accfc0131c535080fb120b55de64927
Harrison <monteiro4100@gmail.com>
2022-09-30 16:42:12 -0300

    requisito 2

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 class WalletForm extends Component {
   render() {
     return (
-      <div>WalletForm</div>
+      <div>
+        WalletForm
+        <input data-testid="value-input" type="text" />
+        <input data-testid="description-input" type="text" />
+        <select data-testid="currency-input">
+          {/* <option> {currencies}</option> */}
+        </select>
+      </div>
     );
   }
 }
