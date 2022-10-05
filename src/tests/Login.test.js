3bebe438cbdf4862c8af26f3d95c68cd889b6502
Harrison <monteiro4100@gmail.com>
2022-10-05 18:28:58 -0300

    requisito 6

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,14 +1,46 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import Header from '../components/Header';
+import renderWithRedux from './helpers/renderWithRouter';
 
 describe('testando Login', () => {
-  test('testa se aparece os campos', () => {
-    render(<App />);
+  test('testa se aparece os campos de login', () => {
+    renderWithRedux(<App />);
     const inputs = screen.getAllByRole('input');
-    const btn = screen.getByText(/entrar/i);
 
     expect(inputs[0]).toBeInTheDocument();
     expect(inputs[1]).toBeInTheDocument();
-    expect(btn).toBeInTheDocument();
+
+    userEvent.type(inputs[0], 'alguem@alguem.com');
+    userEvent.type(inputs[1], '123456');
+
+    // expect(btn).toBeInTheDocument();
+  });
+
+  test('testa o wallet form', () => {
+    renderWithRedux(<App />);
+    const inputs = screen.getAllByRole('input');
+
+    expect(inputs[0]).toBeInTheDocument();
+    expect(inputs[1]).toBeInTheDocument();
+
+    userEvent.type(inputs[0], 'alguem@alguem.com');
+    userEvent.type(inputs[1], '123456');
+
+    const btn = screen.getByText(/entrar/i);
+    userEvent.click(btn);
+
+    const input = screen.getAllByRole('input');
+    expect(input[0]).toBeInTheDocument();
+    expect(input[1]).toBeInTheDocument();
+  });
+  test('testa o header', () => {
+    renderWithRedux(<Header />);
+    const paragrafos = screen.getAllByRole('paragraph');
+
+    expect(paragrafos[0]).toBeInTheDocument();
+    expect(paragrafos[1]).toBeInTheDocument();
+    expect(total()).toBeCalledTimes(1);
   });
 });
---
+++ src/tests/helpers/renderWithRouter.js
@@ -0,0 +1,19 @@
+// src/App.test.js
+// demais imports...
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import clickReducer from '../../redux/reducers';
+
+const renderWithRedux = (
+  component,
+  {
+    initialState,
+    store = createStore(combineReducers({ clickReducer }), initialState),
+  } = {},
+) => ({
+  ...render(<Provider store={ store }>{component}</Provider>),
+  store,
+});
+
+export default renderWithRedux;
